ad6c613c16d54117af0f77dd2adf6c1ff9942b24
Pablo Arce <pablo.arce@bioinfomgp.org>
2014-04-02 09:28:26 +0200

    clean some code

--- bioformats/src/test/java/org/opencb/commons/bioformats/variant/annotators/VariantCaseControlDominantAnnotatorTest.java
+++ bioformats/src/test/java/org/opencb/commons/bioformats/variant/annotators/VariantCaseControlDominantAnnotatorTest.java
@@ -1,12 +1,10 @@
 package org.opencb.commons.bioformats.variant.annotators;
 
-import mockit.Deencapsulation;
 import mockit.Mocked;
 import mockit.NonStrictExpectations;
 import org.junit.Before;
 import org.junit.Test;
 import org.opencb.commons.bioformats.pedigree.Condition;
-import org.opencb.commons.bioformats.pedigree.Individual;
 import org.opencb.commons.bioformats.pedigree.Pedigree;
 import org.opencb.commons.bioformats.variant.Variant;
 import org.opencb.commons.test.GenericTest;
@@ -21,10 +19,6 @@ import static org.junit.Assert.assertTrue;
 public class VariantCaseControlDominantAnnotatorTest extends GenericTest {
     @Mocked
     Pedigree pedigree;
-    @Mocked Individual ind1;
-    @Mocked Individual ind2;
-    @Mocked Individual ind3;
-    @Mocked Individual ind4;
 
     // variants to do the tests
     private Variant noAllelesInCases = new Variant("chr1", 0, null, null);
@@ -36,8 +30,8 @@ public class VariantCaseControlDominantAnnotatorTest extends GenericTest {
     private Variant allCasesMV = new Variant("chr1", 0, null, null);
     private Variant allControlsMV = new Variant("chr1", 0, null, null);
 
-    private final String sample1 = "A";
-    private final String sample2 = "B";
+    private final String case1 = "A";
+    private final String case2 = "B";
     private final String control1 = "C1";
     private final String control2 = "C2";
 
@@ -48,41 +42,17 @@ public class VariantCaseControlDominantAnnotatorTest extends GenericTest {
     private Map<String, String> genotype10;
     private Map<String, String> genotype12;
 
-    private final Set<String> allSamples = new HashSet<>();
-    private final Set<String> samples1to3 = new HashSet<>();
-
     @Before
     public void setUp() throws Exception {
         super.setUp();
-        // samples
-        allSamples.add(sample1);
-        allSamples.add(sample2);
-        allSamples.add(control1);
-        allSamples.add(control2);
-
         new NonStrictExpectations() {{
-            // record pedigree and individuals
-            ind1.getCondition(); result = Condition.AFFECTED;
-            ind2.getCondition(); result = Condition.AFFECTED;
-            ind3.getCondition(); result = Condition.UNAFFECTED;
-            ind4.getCondition(); result = Condition.UNAFFECTED;
-//            Map<String, Individual> affectedIndividuals = new HashMap<>();
-//            affectedIndividuals.put("A", ind1);
-//            affectedIndividuals.put("B", ind2);
-//            Map<String, Individual> controlIndividuals = new HashMap<>();
-//            controlIndividuals.put("C1", ind3);
-//            controlIndividuals.put("C2", ind4);
+            // record pedigree
             Set<String> controlIndividuals = new HashSet<>();
-            controlIndividuals.add("C1");
-            controlIndividuals.add("C2");
+            controlIndividuals.add(control1);
+            controlIndividuals.add(control2);
             Set<String> affectedIndividuals = new HashSet<>();
-            affectedIndividuals.add("A");
-            affectedIndividuals.add("B");
-//            pedigree.getIndividuals(); result = individuals;
-//            pedigree.getIndividual("A"); result = ind1;
-//            pedigree.getIndividual("B"); result = ind2;
-//            pedigree.getIndividual("C1"); result = ind3;
-//            pedigree.getIndividual("C2"); result = ind4;
+            affectedIndividuals.add(case1);
+            affectedIndividuals.add(case2);
             pedigree.getIndividuals(Condition.UNAFFECTED); result = controlIndividuals;
             pedigree.getIndividuals(Condition.AFFECTED); result = affectedIndividuals;
         }};
@@ -101,51 +71,59 @@ public class VariantCaseControlDominantAnnotatorTest extends GenericTest {
         genotype10.put(Variant.GENOTYPE_TAG, "1/0");
         genotype12.put(Variant.GENOTYPE_TAG, "1/2");
 
+        // cs1  cs2  cn1  cn2
         // 0/0  0/0  0/1  0/0
-        noAllelesInCases.addSampleData(sample1, genotype00);
-        noAllelesInCases.addSampleData(sample2, genotype00);
+        noAllelesInCases.addSampleData(case1, genotype00);
+        noAllelesInCases.addSampleData(case2, genotype00);
         noAllelesInCases.addSampleData(control1, genotype01);
         noAllelesInCases.addSampleData(control2, genotype00);
 
+        // cs1  cs2  cn1  cn2
         // 0/1  ./.  0/0  0/1
-        oneAlleleInControl.addSampleData(sample1, genotype00);
-        oneAlleleInControl.addSampleData(sample2, genotypeMissingValue);
+        oneAlleleInControl.addSampleData(case1, genotype00);
+        oneAlleleInControl.addSampleData(case2, genotypeMissingValue);
         oneAlleleInControl.addSampleData(control1, genotype00);
         oneAlleleInControl.addSampleData(control2, genotype01);
 
+        // cs1  cs2  cn1  cn2
         // 0/1  0/0  0/0  0/0
-        oneAlleleNotInControl.addSampleData(sample1, genotype01);
-        oneAlleleNotInControl.addSampleData(sample2, genotype00);
+        oneAlleleNotInControl.addSampleData(case1, genotype01);
+        oneAlleleNotInControl.addSampleData(case2, genotype00);
         oneAlleleNotInControl.addSampleData(control1, genotype00);
         oneAlleleNotInControl.addSampleData(control2, genotype00);
 
+        // cs1  cs2  cn1  cn2
         // 0/1  0/2  0/0  0/0
-        twoAllelesNotInControl.addSampleData(sample1, genotype01);
-        twoAllelesNotInControl.addSampleData(sample2, genotype02);
+        twoAllelesNotInControl.addSampleData(case1, genotype01);
+        twoAllelesNotInControl.addSampleData(case2, genotype02);
         twoAllelesNotInControl.addSampleData(control1, genotype00);
         twoAllelesNotInControl.addSampleData(control2, genotype00);
 
+        // cs1  cs2  cn1  cn2
         // 1/2  0/0  0/1  0/0
-        twoAllelesOneInControl.addSampleData(sample1, genotype12);
-        twoAllelesOneInControl.addSampleData(sample2, genotype00);
+        twoAllelesOneInControl.addSampleData(case1, genotype12);
+        twoAllelesOneInControl.addSampleData(case2, genotype00);
         twoAllelesOneInControl.addSampleData(control1, genotype01);
         twoAllelesOneInControl.addSampleData(control2, genotype00);
 
+        // cs1  cs2  cn1  cn2
         // 1/2  ./.  1/0  0/2
-        twoAllelesBothInControl.addSampleData(sample1, genotype12);
-        twoAllelesBothInControl.addSampleData(sample2, genotypeMissingValue);
+        twoAllelesBothInControl.addSampleData(case1, genotype12);
+        twoAllelesBothInControl.addSampleData(case2, genotypeMissingValue);
         twoAllelesBothInControl.addSampleData(control1, genotype10);
         twoAllelesBothInControl.addSampleData(control2, genotype02);
 
+        // cs1  cs2  cn1  cn2
         // ./.  ./.  0/0  0/0
-        allCasesMV.addSampleData(sample1, genotypeMissingValue);
-        allCasesMV.addSampleData(sample2, genotypeMissingValue);
+        allCasesMV.addSampleData(case1, genotypeMissingValue);
+        allCasesMV.addSampleData(case2, genotypeMissingValue);
         allCasesMV.addSampleData(control1, genotype00);
         allCasesMV.addSampleData(control2, genotype00);
 
+        // cs1  cs2  cn1  cn2
         // 0/1  0/0 ./.  ./.
-        allControlsMV.addSampleData(sample1, genotype01);
-        allControlsMV.addSampleData(sample2, genotype00);
+        allControlsMV.addSampleData(case1, genotype01);
+        allControlsMV.addSampleData(case2, genotype00);
         allControlsMV.addSampleData(control1, genotypeMissingValue);
         allControlsMV.addSampleData(control2, genotypeMissingValue);
     }
@@ -167,9 +145,10 @@ public class VariantCaseControlDominantAnnotatorTest extends GenericTest {
         annotator.annot(variantsToBeFiltered);
 
         System.out.println("--------------- TEST 1 ---------------");
+        System.out.println("case1\t\tcase2\t\tcontrol1\tcontrol2\tSCORE");
         for(Variant variante: variantsToBeFiltered){
-            System.out.println(variante.getSampleData(sample1) + "\t" +
-                    variante.getSampleData(sample2) + "\t" + variante.getSampleData(control1) + "\t" +
+            System.out.println(variante.getSampleData(case1) + "\t" +
+                    variante.getSampleData(case2) + "\t" + variante.getSampleData(control1) + "\t" +
                     variante.getSampleData(control2) + "\t" +
                     variante.getAttribute(VariantCaseControlDominantAnnotator.DCC_SCORE_TAG) + "\t");
         }
@@ -196,9 +175,10 @@ public class VariantCaseControlDominantAnnotatorTest extends GenericTest {
         annotator.annot(variantsToBeFiltered2);
 
         System.out.println("--------------- TEST 2 ---------------");
+        System.out.println("case1\t\tcase2\t\tcontrol1\tcontrol2\tSCORE");
         for(Variant variante: variantsToBeFiltered2){
-            System.out.println(variante.getSampleData(sample1) + "\t" +
-                    variante.getSampleData(sample2) + "\t" + variante.getSampleData(control1) + "\t" +
+            System.out.println(variante.getSampleData(case1) + "\t" +
+                    variante.getSampleData(case2) + "\t" + variante.getSampleData(control1) + "\t" +
                     variante.getSampleData(control2) + "\t" +
                     variante.getAttribute(VariantCaseControlDominantAnnotator.DCC_SCORE_TAG) + "\t");
         }
